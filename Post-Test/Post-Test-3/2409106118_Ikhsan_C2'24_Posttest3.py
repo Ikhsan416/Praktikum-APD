d4cec224b1a42056216135c678dff7710be80017
118Ikhsan <you@example.com>
2024-09-23 16:24:50 +0800

    Posttest_3

---
+++ Post-Test/Post-Test-3/2409106118_Ikhsan_C2'24_Posttest3.py
@@ -0,0 +1,24 @@
+merkmobil = input("Masukkan Merk Mobil Yang Ingin Anda Beli:..!")
+harga = int(input("Masukkan Harga Mobil:..!"))
+harga = harga
+if merkmobil == "tesla":
+    diskon = harga * 0.17
+    bayartotal = harga - diskon
+    print(
+        f"{merkmobil},{bayartotal}. Terimakasih Telah Berbelanja"
+        )
+elif merkmobil == "toyota":
+    diskon = harga * 0.21
+    bayartotal = harga - diskon
+    print(
+        f"{merkmobil},{bayartotal}. Terimakasih Telah Berbelanja"
+        )
+elif merkmobil == "hyundai":
+    diskon = harga * 0.23
+    bayartotal = harga - diskon
+    print(
+        f"{merkmobil},{bayartotal}. Terimakasih Telah Berbelanja"
+        )
+else:
+    print("Mobil Yang Anda Cari Tidak Ada!")
+    print("Terimakasih Telah Datang Kemari")
