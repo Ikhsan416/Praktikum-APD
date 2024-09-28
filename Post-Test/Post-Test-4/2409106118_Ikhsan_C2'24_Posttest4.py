86749cb823aa311eb93a8c523a5243d2585ef4c3
118Ikhsan <you@example.com>
2024-09-29 01:01:29 +0800

    Update Tugas Posttest4

--- Post-Test/Post-Test-4/2409106118_Ikhsan_C2'24_Posttest4.py
+++ Post-Test/Post-Test-4/2409106118_Ikhsan_C2'24_Posttest4.py
@@ -1,3 +1,7 @@
+# Nama  :   Ikhsan
+# Nim   :   2409106118
+# Kelas :   C2'24
+
 loop = 0
 while loop < 3:
     print("Masukkan Nama Anda:")
@@ -5,6 +9,13 @@ while loop < 3:
     print("Masukkan Password Anda:")
     password = int(input())
     if nama == "ikhsan" and password == 118:
+        print("Apa Anda Ingin Mengakhiri Program Ini:(lanjut / berhenti)")
+        program = input()
+        if program == "berhenti":
+            print("Program Anda Telah Selesai!")
+            break
+        else:
+            print("Program Di Lanjut!")
         print("Masukkan Merk Mobil Yang Ingin Anda Beli:..!")
         merkmobil = input()
         print("Masukkan Harga Mobil:..!")
@@ -16,6 +27,7 @@ while loop < 3:
             print(merkmobil)
             print(bayartotal)
             print("Terimakasih Telah Berbelanja")
+            break
         else:
             if merkmobil == "toyota":
                 diskon = harga * 0.21
@@ -23,6 +35,7 @@ while loop < 3:
                 print(merkmobil)
                 print(bayartotal)
                 print("Terimakasih Telah Berbelanja")
+                break
             else:
                 if merkmobil == "hyundai":
                     diskon = harga * 0.23
@@ -30,16 +43,18 @@ while loop < 3:
                     print(merkmobil)
                     print(bayartotal)
                     print("Terimakasih Telah Berbelanja")
+                    break
                 else:
                     print("Mobil Yang Anda Cari Tidak Ada!")
                     print("Terimakasih Telah Datang Kemari")
+                    break
     else:
         print("Apa Anda Ingin Mengakhiri Program Ini:(lanjut / tidak)")
         program = input()
         if program == "lanjut":
             print("Program Anda Telah Selesai!")
+            break
         else:
             loop = loop + 1
             print("Batas Percobaan Yang Tersisa Tinggal:!")
             print(loop)
-print("Batas Pengulangan Yang Anda Miliki Telah Habis, Terimakasih Telah Mencoba!!")
