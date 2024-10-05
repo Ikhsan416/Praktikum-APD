071853547de8c8829a80982f3f53fd1297d9cb37
118Ikhsan <you@example.com>
2024-10-05 22:07:50 +0800

    Update_Posttest5

--- Post-Test/Post-Test-5/2409106118_Ikhsan_C2'24_Posttest5.py
+++ Post-Test/Post-Test-5/2409106118_Ikhsan_C2'24_Posttest5.py
@@ -41,10 +41,10 @@ while True:
                     print(f"\nSelamat datang {Username}!")
                     print("―――Silakan pilih aksi!―――")
                     if akun[2] == 'admin':
-                        print("1. Tambah tiket konser")
-                        print("2. Lihat tiket konser")
-                        print("3. Edit tiket konser")
-                        print("4. Hapus tiket konser")
+                        print("1. Tambah konser")
+                        print("2. Lihat konser")
+                        print("3. Edit konser")
+                        print("4. Hapus konser")
                     else:
                         print("1. Beli tiket konser")
                         print("2. Lihat tiket konser yang sudah dibeli")
@@ -57,78 +57,79 @@ while True:
                     if status == "1":
                         if akun[2] == 'admin':
                             judul_konser = input("Judul Konser: ")
+                            lokasi_konser = input("Lokasi Konser: ")
                             harga_tiket = input("Harga Tiket: ")
-                            tiket_konser.append([judul_konser, harga_tiket])  # Menambahkan tiket ke dalam list
-                            print("Tiket konser berhasil ditambahkan!\n")
+                            tiket_konser.append([judul_konser, lokasi_konser, harga_tiket])  # Menambahkan tiket ke dalam list
+                            print("Konser berhasil ditambahkan!\n")
                         else:
                             judul_konser = input("Judul Konser: ")
                             for tiket in tiket_konser:
                                 if tiket[0] == judul_konser:
-                                    akun[3].append([judul_konser, tiket[1]])  # Menambahkan tiket ke dalam list
+                                    akun[3].append([judul_konser, tiket[1], tiket[2]])  # Menambahkan tiket ke dalam list
                                     print("Tiket konser berhasil dibeli!\n")
                                     break
                             else:
-                                print("Tiket konser tidak tersedia.\n")
+                                print("Konser tidak tersedia.\n")
 
                     elif status == "2":
                         if akun[2] == 'admin':
                             for tiket in tiket_konser:  
-                                print(f"Judul Konser: {tiket[0]}\nHarga Tiket: {tiket[1]}\n")
+                                print(f"Judul Konser: {tiket[0]}\nLokasi Konser: {tiket[1]}\nHarga Tiket: {tiket[2]}\n")
                             if not tiket_konser:
-                                print("Opps, saat ini belum ada tiket konser, silahkan tambah tiket terlebih dahulu.\n")
+                                print("Opps, saat ini belum ada konser, silahkan tambah konser terlebih dahulu.\n")
                         else:
                             for tiket in akun[3]:  
-                                print(f"Judul Konser: {tiket[0]}\nHarga Tiket: {tiket[1]}\n")
+                                print(f"Judul Konser: {tiket[0]}\nLokasi Konser: {tiket[1]}\nHarga Tiket: {tiket[2]}\n")
                             if not akun[3]:
                                 print("Opps, saat ini kamu belum punya tiket, silahkan beli tiket terlebih dahulu.\n")
-                            
                     elif status == "3":
                         if akun[2] == 'admin':
                             if not tiket_konser:
-                                print("Tidak ada tiket konser yang bisa diedit.")
+                                print("Tidak ada konser yang bisa diedit.")
                             else:
-                                edit = int(input("Tiket konser nomor berapa yang ingin kamu edit: ")) - 1
+                                edit = int(input("Konser nomor berapa yang ingin kamu edit: ")) - 1
                                 if 0 <= edit < len(tiket_konser):
                                     judul_baru = input("Masukkan judul yang baru: ")
+                                    lokasi_baru = input("Masukkan lokasi yang baru: ")
                                     harga_baru = input("Masukkan harga yang baru: ")
-                                    print("Apa kamu yakin ingin mengedit tiket konser ?")
+                                    print("Apa kamu yakin ingin mengedit konser ?")
                                     print("1. Iya")
                                     print("2. Tidak")
                                     memastikan_edit = input("Pilih: ")
                                     if memastikan_edit == "1":
-                                        tiket_konser[edit] = [judul_baru, harga_baru]  # Mengedit tiket konser
-                                        print("Tiket konser yang kamu pilih sudah di edit ya!\n")
+                                        tiket_konser[edit] = [judul_baru, lokasi_baru, harga_baru]  # Mengedit konser
+                                        print("Konser yang kamu pilih sudah di edit ya!\n")
                                     elif memastikan_edit == "2":
-                                        print("Aksi untuk mengedit tiket konser dibatalkan")
+                                        print("Aksi untuk mengedit konser dibatalkan")
                                     else:
                                         print("Mohon pilih '1' atau '2'")
                                 else:
-                                    print("Tidak ada nomor tiket konser yang kamu maksud, silahkan input ulang.\n")
+                                    print("Tidak ada nomor konser yang kamu maksud, silahkan input ulang.\n")
                         else:
-                            print("Anda tidak memiliki akses untuk mengedit tiket konser.\n")
+                            print("Anda tidak memiliki akses untuk mengedit konser.\n")
 
                     elif status == "4":
                         if akun[2] == 'admin':
                             if not tiket_konser:
-                                print("Tidak ada tiket konser yang bisa dihapus.")
+                                print("Tidak ada konser yang bisa dihapus.")
                             else:
-                                hapus = int(input("Tiket konser nomor berapa yang ingin dihapus: ")) - 1
+                                hapus = int(input("Konser nomor berapa yang ingin dihapus: ")) - 1
                                 if 0 <= hapus < len(tiket_konser):
-                                    print(f"Apa kamu yakin ingin menghapus tiket konser ? ")
+                                    print(f"Apa kamu yakin ingin menghapus konser ? ")
                                     print("1. Iya")
                                     print("2. Tidak")
                                     memastikan_hapus = input("Pilih: ")
                                     if memastikan_hapus == "1":
-                                        del tiket_konser[hapus]  # Menghapus tiket konser dari list
-                                        print("Tiket konser yang kamu pilih sudah dihapus!\n")
+                                        del tiket_konser[hapus]  # Menghapus konser dari list
+                                        print("Konser yang kamu pilih sudah dihapus!\n")
                                     elif memastikan_hapus == "2":
-                                        print("Aksi untuk menghapus tiket konser dibatalkan")
+                                        print("Aksi untuk menghapus konser dibatalkan")
                                     else:
                                         print("Mohon pilih '1' atau '2'")
                                 else:
-                                    print("Tidak ada nomor tiket konser yang kamu maksud, silahkan input ulang.\n")
+                                    print("Tidak ada nomor konser yang kamu maksud, silahkan input ulang.\n")
                         else:
-                            print("Anda tidak memiliki akses untuk menghapus tiket konser.\n")
+                            print("Anda tidak memiliki akses untuk menghapus konser.\n")
 
                     elif status == "5":
                         print("Aplikasi Pembelian Tiket Konser ditutup.\n")
