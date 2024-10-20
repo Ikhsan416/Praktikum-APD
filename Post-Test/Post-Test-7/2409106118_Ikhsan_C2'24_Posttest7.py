3841cae60933b85be473c6c1eea1e67574df69ba
118Ikhsan <you@example.com>
2024-10-20 20:46:57 +0800

    Posttest 7 Terakhir

--- Post-Test/Post-Test-7/2409106118_Ikhsan_C2'24_Posttest7.py
+++ Post-Test/Post-Test-7/2409106118_Ikhsan_C2'24_Posttest7.py
@@ -23,6 +23,7 @@ class style():
     CBLINK    = '\33[5m'
     CBLINK2   = '\33[6m'
     CSELECTED = '\33[7m'
+    
     CBLACK  = '\33[30m'
     CRED    = '\33[31m' 
     CGREEN  = '\33[32m'
@@ -31,6 +32,7 @@ class style():
     CVIOLET = '\33[35m'
     CBEIGE  = '\33[36m'
     CWHITE  = '\33[37m'
+    
     CBLACKBG  = '\33[40m'
     CREDBG    = '\33[41m'
     CGREENBG  = '\33[42m'
@@ -39,6 +41,7 @@ class style():
     CVIOLETBG = '\33[45m'
     CBEIGEBG  = '\33[46m'
     CWHITEBG  = '\33[47m'
+    
     CGREY    = '\33[90m'
     CRED2    = '\33[91m'
     CGREEN2  = '\33[92m'
@@ -47,6 +50,7 @@ class style():
     CVIOLET2 = '\33[95m'
     CBEIGE2  = '\33[96m'
     CWHITE2  = '\33[97m'
+    
     CGREYBG    = '\33[100m'
     CREDBG2    = '\33[101m'
     CGREENBG2  = '\33[102m'
